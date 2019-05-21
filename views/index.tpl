9e6249339123128e8c8851b1cfcbc3af589bc408
Regina Blagotinšek <regina.blagotinsek@student.fmf.uni-lj.si>
2019-05-21 19:59:55 +0200

    konec vaj

--- views/index.tpl
+++ views/index.tpl
@@ -13,7 +13,7 @@
   <img src="img/10.jpg" alt="obesanje">
 
   <form action="/igra/" method="post">
-    <button type="submit">Nova igra</button>
+      <button type="submit">Nova igra</button>
   </form>
 </body>
 
--- vislice.py
+++ vislice.py
@@ -2,17 +2,22 @@ import bottle
 import model
 
 vislice = model.Vislice()
-id = vislice.nova_igra()
-igra, stanje = vislice.igre[id]
+#id = vislice.nova_igra()
+#igra, stanje = vislice.igre[id]
+#vislice.ugibaj(id, 'A')
+#vislice.ugibaj(id, 'K')
+#vislice.ugibaj(id, 'U')
+#vislice.ugibaj(id, 'L')
+
 
 @bottle.get('/')
 def index():
     return bottle.template('index.tpl') 
 
 
-@bottle.get('/igra/')
-def testigra():
-    return bottle.template('igra.html', id_igra=id, igra=igra, stanje=stanje) 
+#@bottle.get('/igra/')
+#def testigra():
+#    return bottle.template('igra.html', id_igre=id, igra=igra, stanje=stanje) S TEM SMO SE NEKAJ IGRALI
 
 #v brskalnik napisemo localhost:8080 in tam je igrica
 #ta funkcija pove da na nekih rootih(/) prikaze slikico:
@@ -21,4 +26,25 @@ def testigra():
 def slike(ime):
     return bottle.static_file(ime, root = 'img') 
 
+@bottle.get('/igra/')
+def nova_igra():
+    id = vislice.nova_igra()
+    bottle.redirect('/igra/{0}/'.format(id))
+
+@bottle.get('/igra/<id_igre:int>/')
+def pokazi_igro(id_igre):
+    igra, stanje = vislice.igre[id_igre]
+    return bottle.template('igra.html', id_igre=id_igre, igra=igra, stanje=stanje)
+
+@bottle.post('/igra/<id_igre:int>/')
+def ugibaj(id_igre):
+    crka = bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra/{0}/'.format(id_igre))
+    print(crka)
+
+
+
+
 bottle.run(reloader=True, debug=True)
+
